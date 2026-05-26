e6fd98b3b55bfefb4253a209f178ccb313f84ba0
kjh27ss <kjh27ss@naver.com>
2026-05-26 14:41:10 +0900

    footer 작업

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+const header = document.querySelector(".header");
+
+window.addEventListener("scroll", function () {
+
+  if(window.scrollY > 50){
+    header.classList.add("active");
+  }else{
+    header.classList.remove("active");
+  }
+});
+
 const mainSwiper = new Swiper(".banner_wrap .slide_mask", {
   slidesPerView: 1,
   loop: true,
@@ -10,24 +21,25 @@ const mainSwiper = new Swiper(".banner_wrap .slide_mask", {
 });
 
 const bestSwiper = new Swiper(".best_wrap .slide_mask", {
-  slidesPerView: 1,
-  spaceBetween: 20,
+  slidesPerView: 2,
+  spaceBetween: 10,
   loop: true,
-
   autoplay: {
     delay: 5000,
   },
-
   breakpoints: {
     326: {
         slidesPerView: 2,
+        spaceBetween: 10,
     },
     768: {
       slidesPerView: 3,
+      spaceBetween: 20,
     },
 
     1200: {
       slidesPerView: 4,
+      spaceBetween: 20,
     },
   },
 });
